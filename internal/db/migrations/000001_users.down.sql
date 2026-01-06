3f8cfaa348679e6e2ded12b15dbc6a71a0dbb72a
nhathuych <nhathuych@gmail.com>
2026-01-06 19:09:24 +0700

    improve users migration

--- internal/db/migrations/000001_users.down.sql
+++ internal/db/migrations/000001_users.down.sql
@@ -1,1 +1,8 @@
-DROP TABLE if EXISTS users;
+-- Drop trigger
+DROP TRIGGER IF EXISTS users_set_updated_at ON users;
+
+-- Drop function
+DROP FUNCTION IF EXISTS update_user_updated_at();
+
+-- Drop table
+DROP TABLE IF EXISTS users;
